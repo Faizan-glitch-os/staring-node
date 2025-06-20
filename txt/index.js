61ddcaee672c906f41b9e8f6da86ef3cab8329f4
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-06-20 15:07:32 +0500

    server created and started

--- txt/index.js
+++ txt/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const http = require("http");
 
 // const readFile = fs.readFileSync("./read-this.txt", "utf-8");
 
@@ -12,28 +13,36 @@ const fs = require("fs");
 
 // asyncFileReadAndWrite
 
-fs.readFile("./start.txt", "utf-8", (error, data1) => {
-  if (error) {
-    return console.log(`Error occured ${error}`);
-  }
-  console.log(data1);
+// fs.readFile("./start.txt", "utf-8", (error, data1) => {
+//   if (error) {
+//     return console.log(`Error occured ${error}`);
+//   }
+//   console.log(data1);
 
-  fs.readFile(`./${data1}.txt`, "utf-8", (error, data2) => {
-    if (error) {
-      return console.log(`Error occured at second callback ${error}`);
-    }
-    console.log(data2);
+//   fs.readFile(`./${data1}.txt`, "utf-8", (error, data2) => {
+//     if (error) {
+//       return console.log(`Error occured at second callback ${error}`);
+//     }
+//     console.log(data2);
 
-    fs.writeFile("./myFile.txt", "kaka", "utf-8", (error) => {
-      if (error) {
-        return console.log(`Error writting the file ${error}`);
-      }
+//     fs.writeFile("./myFile.txt", "kaka", "utf-8", (error) => {
+//       if (error) {
+//         return console.log(`Error writting the file ${error}`);
+//       }
 
-      fs.readFile("./myFile.txt", "utf-8", (error, data4) => {
-        console.log(data4);
-      });
-    });
-  });
+//       fs.readFile("./myFile.txt", "utf-8", (error, data4) => {
+//         console.log(data4);
+//       });
+//     });
+//   });
+// });
+
+// console.log("All are async file read and write");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello first request");
 });
 
-console.log("All are async file read and write");
+server.listen(3000, () => {
+  console.log("Server started at localhost 3000");
+});
