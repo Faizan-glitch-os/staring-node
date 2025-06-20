c05e4c791b5d2773cd692e18b98001753d295544
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-06-20 14:35:08 +0500

    practiced async file read and write

--- txt/index.js
+++ txt/index.js
@@ -1,11 +1,39 @@
 const fs = require("fs");
 
-const readFile = fs.readFileSync("./read-this.txt", "utf-8");
+// const readFile = fs.readFileSync("./read-this.txt", "utf-8");
 
-const newWrite = `This is new writting to the file: ${readFile}`;
+// const newWrite = `This is new writting to the file: ${readFile}`;
 
-const writeFile = fs.writeFileSync("./output.txt", newWrite);
+// s.writeFileSync("./output.txt", newWrite);
 
-const outputRead = fs.readFileSync("./output.txt", "utf-8");
+// const outputRead = fs.readFileSync("./output.txt", "utf-8");
 
-console.log(outputRead);
+// console.log(outputRead);
+
+// asyncFileReadAndWrite
+
+fs.readFile("./start.txt", "utf-8", (error, data1) => {
+  if (error) {
+    return console.log(`Error occured ${error}`);
+  }
+  console.log(data1);
+
+  fs.readFile(`./${data1}.txt`, "utf-8", (error, data2) => {
+    if (error) {
+      return console.log(`Error occured at second callback ${error}`);
+    }
+    console.log(data2);
+
+    fs.writeFile("./myFile.txt", "kaka", "utf-8", (error) => {
+      if (error) {
+        return console.log(`Error writting the file ${error}`);
+      }
+
+      fs.readFile("./myFile.txt", "utf-8", (error, data4) => {
+        console.log(data4);
+      });
+    });
+  });
+});
+
+console.log("All are async file read and write");
